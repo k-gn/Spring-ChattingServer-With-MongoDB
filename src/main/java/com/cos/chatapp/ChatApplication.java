32c4dca993e914b1ce62ef5dd50ed44a830baefb
k-gn <rlarbska97@gmail.com>
2022-03-22 13:58:47 +0900

    mongodb config(yml)

--- src/main/java/com/cos/chatapp/ChatApplication.java
+++ src/main/java/com/cos/chatapp/ChatApplication.java
@@ -3,6 +3,7 @@ package com.cos.chatapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 기본적으로 springboot 는 비동기 서버를 netty로 사용한다.
 @SpringBootApplication
 public class ChatApplication {
 
