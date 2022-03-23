46fb215e578b0a53272117e5c294ba9ebdc768f9
k-gn <rlarbska97@gmail.com>
2022-03-23 15:08:56 +0900

    add chat entity, repository

---
+++ src/main/java/com/cos/chatapp/entity/Chat.java
@@ -0,0 +1,24 @@
+package com.cos.chatapp.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collation = "chat")
+public class Chat {
+
+    // MongoDB의 PK 값은 해쉬값 -> 타입이 bson 타입인데 이걸 자바에서 String 으로 받으면 객체가 아니라 해시값만 받아진다.
+    @Id
+    private String id;
+
+    private String msg;
+
+    private String sender;
+
+    private String receiver;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/cos/chatapp/repository/ChatRepository.java
@@ -0,0 +1,16 @@
+package com.cos.chatapp.repository;
+
+import com.cos.chatapp.entity.Chat;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    // Flux 흐름 -> 연결을 끊지 않고 지속적으로 연결하겠다는 의미 => response를 유지하면서 데이터를 계속 흘려보낼 수 있다.
+    // 이때 http 프로토콜을 사용하면 지속적인 처리가 안되서 SSE 프로토콜을 사용해야 한다. (응답 연결을 유지시킨다.)
+    @Tailable // 커서를 안닫고 계속 유지한다.
+    @Query("{sender: ?0, receiver: ?1}")
+    Flux<Chat> msgFindBySender(String sender, String receiver);
+}
