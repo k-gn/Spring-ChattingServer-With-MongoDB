61b390f00fa9a1be9a736623647311a430afd9f9
k-gn <rlarbska97@gmail.com>
2022-03-23 18:32:47 +0900

    초기화 작업 및 메시지 요청 진행

--- chat-front/js/chat.js
+++ chat-front/js/chat.js
@@ -2,37 +2,75 @@ const eventSource = new EventSource("http://localhost:8080/sender/ssar/receiver/
 eventSource.onmessage = (event) => {
     const data = JSON.parse(event.data);
     console.log(data);
+    initMessage(data);
 }
 
 document.querySelector("#chat-send").addEventListener("click", () => {
 
-    let chatBox = document.querySelector("#chat-box");
-    let msgBox = document.querySelector("#chat-outgoing-msg");
-
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg";
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgBox.value);
-    chatBox.append(chatOutgoingBox);
-    msgBox.value = "";
+    addMessage();
 });
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => {
 
     if(e.keyCode === 13) {
-        let chatBox = document.querySelector("#chat-box");
-        let msgBox = document.querySelector("#chat-outgoing-msg");
-
-        let chatOutgoingBox = document.createElement("div");
-        chatOutgoingBox.className = "outgoing_msg";
-        chatOutgoingBox.innerHTML = getMsg(msgBox.value);
-        chatBox.append(chatOutgoingBox);
-        msgBox.value = "";
+        addMessage();
     }
 });
 
-function getSendMsgBox(msg) {
+function addMessage() {
+    let chatBox = document.querySelector("#chat-box");
+    let msgBox = document.querySelector("#chat-outgoing-msg");
+    
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+
+    let chat = {
+        sender : "ssar",
+        receiver : "cos",
+        msg: msgBox.value
+    };
+
+    fetch("http://localhost:8080/chat", {
+        method: "post", 
+        body: JSON.stringify(chat),
+        headers: {
+            "Content-Type":"application/json; charset=utf-8"
+        }
+    });
+
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgBox.value, getNowDate());
+    chatBox.append(chatOutgoingBox);
+    msgBox.value = "";
+} 
+
+function initMessage(historyMsg) {
+    let chatBox = document.querySelector("#chat-box");
+    let msgBox = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+    chatOutgoingBox.innerHTML = getSendMsgBox(historyMsg.msg, getParseNowDate(historyMsg.createdAt));
+    chatBox.append(chatOutgoingBox);
+    msgBox.value = "";
+} 
+
+
+function getSendMsgBox(msg, time) {
     return `<div class="sent_msg">
                 <p>${msg}</p>
-                <span class="time_date"> 11:18 | Today</span>
+                <span class="time_date"> ${time} </span>
             </div>`;
+}
+
+function getNowDate() {
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes() + " | " + (date.getMonth() + 1) + "/" + date.getDate();
+    return now;
+}
+
+function getParseNowDate(dateString) {
+    let date = new Date(Date.parse(dateString));
+    let now = date.getHours() + ":" + date.getMinutes() + " | " + (date.getMonth() + 1) + "/" + date.getDate();
+    return now;
 }
