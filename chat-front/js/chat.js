15269d8a0d11b39a927356ba04216b84db21d947
k-gn <rlarbska97@gmail.com>
2022-03-23 17:03:17 +0900

    add js code

--- chat-front/js/chat.js
+++ chat-front/js/chat.js
@@ -1,11 +1,32 @@
 document.querySelector("#chat-send").addEventListener("click", () => {
-    
-    alert("클릭됨");
+
+    let chatBox = document.querySelector("#chat-box");
+    let msgBox = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+    chatOutgoingBox.innerHTML = getMsg(msgBox.value);
+    chatBox.append(chatOutgoingBox);
+    msgBox.value = "";
 });
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => {
 
     if(e.keyCode === 13) {
-        alert("엔터");
+        let chatBox = document.querySelector("#chat-box");
+        let msgBox = document.querySelector("#chat-outgoing-msg");
+
+        let chatOutgoingBox = document.createElement("div");
+        chatOutgoingBox.className = "outgoing_msg";
+        chatOutgoingBox.innerHTML = getMsg(msgBox.value);
+        chatBox.append(chatOutgoingBox);
+        msgBox.value = "";
     }
 });
+
+function getMsg(msg) {
+    return `<div class="sent_msg">
+                <p>${msg}</p>
+                <span class="time_date"> 11:18 | Today</span>
+            </div>`;
+}
